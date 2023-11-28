7d2138d5c3979dc89aea0e9e3c2079f741abc141
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-28 12:03:58 +0100

    Coloco un botón.

--- src/eventoBoton/EventoBoton.java
+++ src/eventoBoton/EventoBoton.java
@@ -3,6 +3,9 @@ package eventoBoton;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
 public class EventoBoton {
 
@@ -15,6 +18,7 @@ public class EventoBoton {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					System.out.println("Iniciando programa ...");
 					EventoBoton window = new EventoBoton();
 					window.frmVentanaPrincipal.setVisible(true);
 				} catch (Exception e) {
@@ -39,6 +43,10 @@ public class EventoBoton {
 		frmVentanaPrincipal.setTitle("Ventana Principal");
 		frmVentanaPrincipal.setBounds(100, 100, 300, 140);
 		frmVentanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton bBoton = new JButton("Di algo !!!");
+		bBoton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		frmVentanaPrincipal.getContentPane().add(bBoton, BorderLayout.CENTER);
 	}
 
 }
